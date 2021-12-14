ea05e0b20ee789ac48425657008ddb147993ed38
Shay Cojocaru <shayc@outlook.com>
2021-12-14 22:29:52 +0200

    fix: storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,10 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
+    // '@storybook/preset-create-react-app',
   ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
 };
