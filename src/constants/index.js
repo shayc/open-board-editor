8708ede8251b2938119282598be6836e09826356
Shay Cojocaru <shayc@outlook.com>
2022-01-11 00:36:17 +0200

    fix: change app name

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const APP_NAME = 'Communication Board';
+export const APP_NAME = 'Board Editor';
 export const APP_LICENSE = 'MIT';
 export const APP_LICENSE_URL = 'https://opensource.org/licenses/MIT';
 export const APP_AUTHOR = 'Shay C.';
